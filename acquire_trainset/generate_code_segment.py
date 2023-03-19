5ab805b62c66b89edfe45d4758fb20f4bf6113a4
DDizzzy79 <patrick_0429@163.com>
2023-03-19 18:57:22 +0800

    Finished basic of acquiring datasets

--- acquire_trainset/generate_code_segment.py
+++ acquire_trainset/generate_code_segment.py
@@ -1,10 +1,20 @@
+import sys
 import openai
 from tqdm import tqdm
 import config
+from loguru import logger
 
 openai.api_key = config.OPEN_AI_KEY 
 
-def get(payload):
+def setting():
+    # logger.add(
+    #     sys.stderr,
+    #     format="<blue>[+]</blue> {message}",
+    #     level="INFO"
+    # )
+    pass
+
+def get(payload,amount):
     completion = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages = [
@@ -13,25 +23,33 @@ def get(payload):
                 "content": str(payload),
             }
         ],
-        presence_penalty= 2.0,
-        temperature=1.5
-        # n = 5
+        n = int(amount)
     )
-    print(completion.choices[0].message.content)
+    #print(completion.choices[0].message.content)
     return completion
 
 def collect_generated_code(amount_of_time):
     prompt = open("./prompt.txt",'r').read()
+    logger.info("Process started")
     codes = []
-    for _ in tqdm(range(amount_of_time)):
-        respone = get(prompt)
-        generated_code = respone.choices[0].text
-        generated_code = generated_code.replace("code_start", "")
-        generated_code = generated_code.replace("code_end", "")
-        codes.append(generated_code)
-    return codes
+    respone = get(prompt,amount_of_time)
+    logger.info("Connection established")
+    for i in tqdm(range(0,amount_of_time)):
+        try:
+            logger.info(f"working on {i}")
+            generated_code = respone.choices[i].message.content
+            generated_code = generated_code.replace("code_start", "")
+            generated_code = generated_code.replace("code_end", "")
+            generated_code = "#include" + generated_code.split("#include")[-1]
+            codes.append(generated_code)
+            logger.success("Sucess!")
+            return codes
+        except:
+            logger.error("Some error happend.")
+        
 
     
 
 if __name__=="__main__":
+    setting()
     print(collect_generated_code(2))
