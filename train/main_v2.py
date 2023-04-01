38375a59e6b907d244f2c03beeb7ad2318c0d73b
DDizzzy79 <patrick_0429@163.com>
2023-04-01 14:58:12 +0800

    Back....

--- train/main_v2.py
+++ train/main_v2.py
@@ -74,9 +74,6 @@ def finetune_pwnbert(vuln_dir, nvuln_dir, vuln_eval_dir, nvuln_eval_dir, model_n
         logging_strategy="epoch",
         save_strategy="epoch",
         load_best_model_at_end=True,
-        # New added
-        lr_scheduler_type=SchedulerType.LINEAR,
-        weight_decay=0.01,  # Add weight decay
     )
 
     # 使用DataCollatorWithPadding来处理批次的填充
