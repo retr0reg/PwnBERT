69ea9568bdb2f96cf278f586d7b38f0ee28f442f
DDizzzy79 <patrick_0429@163.com>
2023-04-11 21:40:39 +0800

    Fixed bugs

--- retrive_cwe_codes/process_c_file.py
+++ retrive_cwe_codes/process_c_file.py
@@ -111,6 +111,6 @@ if __name__ == "__main__":
     input_dir = ""
     for i in range(1,2+1):
         vuln_amount += process_directory(input_dir=f"juliet-test-suite-c/testcases/CWE121_Stack_Based_Buffer_Overflow/s0{i}", output_dir="outputs/vuln")
-        nvuln_amount += process_directory(input_dir=f"juliet-test-suite-c/testcases/CWE121_Stack_Based_Buffer_Overflow/s0{i}", output_dir="outputs/nvuln", nvuln=True)[0]
+        nvuln_amount += process_directory(input_dir=f"juliet-test-suite-c/testcases/CWE121_Stack_Based_Buffer_Overflow/s0{i}", output_dir="outputs/nvuln", nvuln=True)
         
     print(f"Total {vuln_amount} vuln files were processed, {nvuln_amount} non-vuln file were processed; Totally {vuln_amount+nvuln_amount} files were processed")
